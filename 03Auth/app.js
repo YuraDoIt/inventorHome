625440f66eba48545413f970989d383941311124
Yurii <yura9989@gmail.com>
2021-11-03 10:26:08 +0200

    Add routes

--- 03Auth/app.js
+++ 03Auth/app.js
@@ -1,17 +1,35 @@
 import express from 'express';
 import mongoose from 'mongoose';
-const app =  express();
+import authRole from './AuthRotes';
 
 const port = process.env.PORT || 3000;
 
+const app =  express();
+
 app.use(express.json());
+app.use("/auth", authRole);
 
 const start = () => {
     try{
         mongoose.connect(`mongodb+srv://yura:yura12345@cluster0.an8b0.mongodb.net/Auth?retryWrites=true&w=majority`);
+        
+        app.get('/',function(req,res,next){
+            res.send('Hi')
+            next();
+        });
+        
+        let myFun = function(req,res,next){
+            console.log("New server reload");
+            next();
+        }
+
+        app.use(myFun);
+
         app.listen(port, ()=>{
             console.log(`Server run on port ${port}`)
         });
+
+        
     }
     catch (e) {
         console.log(e);
