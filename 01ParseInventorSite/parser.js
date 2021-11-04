1c2ff5c7e4d8f9b04b99798dac947666c2850ff1
Yurii <yura9989@gmail.com>
2021-11-04 15:22:37 +0200

    create 02 Folder perfect Architecture

--- 01ParseInventorSite/parser.js
+++ 01ParseInventorSite/parser.js
@@ -1,10 +1,11 @@
-import  rp  from 'request-promise';
-import  cheerio from 'cheerio';
+import rp from 'request-promise';
+import cheerio from 'cheerio';
 import fs from 'fs';
 import nodemailer from 'nodemailer';
 
-import { mapToObj } from './mapToObj.mjs';
-import { zipFile } from './zipFile.mjs';
+import { mapToObj } from './mapToObj.js';
+import { zipFile } from './zipFile.js';
+
 
 const url = 'https://inventorsoft.co/careers';
 
@@ -27,13 +28,11 @@ rp(url)
             if(err) throw err;            
         })
 
-    })
-    .then(() => {
         zipFile('vacancy.json','inventorTest.gz');
-    })
-    .then(() => {
+  
         //sendMail();
-    })
+
+    })      
     .catch(function(err){
         return err;
     })
@@ -51,17 +50,54 @@ let mailOption = {
     from: '***',
     to: 'yura9989@gmail.com',
     subject: `Inventor soft parse info about vacancies`,
-    html:`The body of the email goes here in html`,
+    html: `The body of the email goes here in html`,
     attachments: [{
         path: __dirname + "/" + 'inventorTest.gz'
     }]
 
 };
 
-transporter.sendMail(mailOption, function(err, info){
-    if(err){
+transporter.sendMail(mailOption, function (err, info) {
+    if (err) {
         console.log(err)
     } else {
         console.log('Email send to: ' + info.response + " receiver : " + mailOption.to);
     }
 })
+
+function execude(html) {
+
+    const $ = cheerio.load(html);
+    const map = new Map();
+
+    $('.vacancy p', html).each(function (index) {
+        map.set(index + 1, $(this).text());
+    });
+
+    let converted = mapToObj(map);
+    console.log(converted);
+
+    let data = JSON.stringify(converted, null, 2);
+    fs.writeFile('./vacancy.json', data, (err) => {
+        if (err) throw err;
+    })
+}
+
+
+
+// async function fetchData() {
+//     try {
+//         await rp(url);
+
+//         await execude(html);
+
+//         //await zipFile('vacancy.json', 'inventorTest.gz');
+
+//         //await sendMail();
+//     }
+//     catch (e) {
+//         //throw e;
+//     }
+// }
+
+// fetchData();
